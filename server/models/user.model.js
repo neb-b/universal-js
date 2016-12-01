a78e53b7d15bc96892961ab0048e85219f68f0ec
Sergio Prada <chechopradaster@gmail.com>
2016-11-30 23:26:07 -0500

    Extends User model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,9 +4,14 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: { type: 'String', required: true },
+  name: { type: 'String', required: false },
+  email: { type: 'String', required: false },
   fb_id: { type: 'String', required: false },
+  ak_id: { type: 'String', required: false },
+  admin: { type: 'Boolean', default: false, required: true },
+  venue: { type: Schema.Types.ObjectId, ref: 'Venue' },
   dateAdded: { type: 'Date', default: Date.now, required: true },
+  eventsInterested: [ { type: Schema.Types.ObjectId, ref:'Event'}]
 });
 
 UserSchema.statics.createAndSave = Promise.method(function (props) {
