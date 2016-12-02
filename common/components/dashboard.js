0fb8cea2d1bccd07a78565608443bac25f4572e2
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-12-01 21:01:21 -0800

    add current events

--- common/components/dashboard.js
+++ common/components/dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 // import { StyleSheet, css } from 'aphrodite'
 import FacebookEvents from './dashboard/facebook-events'
+import CurrentEvents from './dashboard/current-events'
 
 const Dashboard = ({ createEvent }) => (
   <div>
     <p>View/edit created events, add events from facebook</p>
     <FacebookEvents />
+    <CurrentEvents />
   </div>
 )
 
---
+++ common/components/dashboard/current-events.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { css, StyleSheet } from 'aphrodite'
+
+// will be passed a list of events in props
+const CurrentEvents = () => (
+  <div>
+    <h2>Current Events</h2>
+  </div>
+)
+
+// const styles = StyleSheet.create({
+//
+// })
+
+export default CurrentEvents
