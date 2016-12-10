f20674baafcc4d54a232f719bfadd6020da99e53
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-12-10 15:20:18 -0800

    show account info and add logic to hide dashboard button if not venue

--- common/components/account.js
+++ common/components/account.js
@@ -2,26 +2,31 @@ import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
 import { Link } from 'react-router'
 
-const Account = (props) => (
-  <div>
-    <p className={css(styles.lead)}>
-      This is where all venue account actions happen.
-    </p>
-    <Link to='account/dashboard'>
-      <div className={css(styles.button)}>View Event Dashboard</div>
-    </Link>
-    <p>show this link only if they are venues</p>
-  </div>
-)
+const Account = (props) => {
+  const { account: { name, venue } } = props
+  return (
+    <div>
+      <div className={css(styles.info)}>
+        <h3>Name: { name }</h3>
+      </div>
+      {
+        venue && (
+          <Link to='account/dashboard'>
+            <div className={css(styles.button)}>View Event Dashboard</div>
+          </Link>
+        )
+      }
+    </div>
+  )
+}
 
 const styles = StyleSheet.create({
-  lead: {
-    fontSize: 18,
-    lineHeight: '1.5',
-    margin: '0 0 1.5rem',
-    color: '#555'
+  info: {
+    display: 'inline-block'
   },
   button: {
+    float: 'right',
+    display: 'inline-block',
     width: '50%',
     padding: 10,
     borderRadius: 5,
