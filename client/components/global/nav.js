f12cd103ef8f89fce6d97825451a66de1cc81520
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-12-29 00:02:26 -0500

    fix logout link

--- client/components/global/nav.js
+++ client/components/global/nav.js
@@ -33,7 +33,7 @@ const Nav = () => {
             Hello {name}</span>,
           <a
             key={2}
-            href='logout'
+            href='/logout'
             className={css(styles.link)}>
               Logout
           </a>
